0dfb8c9bacbc07c42732b910a2e7f88a4fc01fd7
Viacheslav <viacheslav@mcublog.xyz>
2019-02-09 20:59:04 +0300

    modified graph_test.py;

--- graph/graph_test.py
+++ graph/graph_test.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
+import math
 
-squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+
+plt.title("Test graph", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Func of value", fontsize = 14)
+x_val = list(range(1, 100))
+y_val = [math.lgamma(x) for x in x_val]
+plt.plot(x_val, y_val)
 plt.show()
