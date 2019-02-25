2866b44a58b52c76f559601e17a21e387d19fc94
Vyacheslav Konovalov <konovalov@healbe.com>
2019-02-25 12:31:51 +0300

    added some comments xml_graph.py;

--- graph/xml_graph/xml_graph.py
+++ graph/xml_graph/xml_graph.py
@@ -11,28 +11,29 @@ plt.title("Temperature graphic", fontsize = 14)
 plt.xlabel("Time", fontsize = 8)
 plt.ylabel("Temperature", fontsize = 12)
 
-x_val = []
-x_tick = []
-y_val = []
+x_val = [] # x value like 1, 2, 3 and etc
+x_tick = [] # x ticks like 10:42:30:513, 10:42:31:513 and etc
+y_val = [] # value of temperature 
 
 for report in root:
-    timestamp = report.find('timestamp')
+    timestamp = report.find('timestamp') # Get node timestamp
     time = timestamp.find('time').text
     x_val.append(int(report.get('id')))
-    x_tick.append(time)
+    x_tick.append(time) # Append time for current id
+    
+    # Get temp from hdc1080 node
     hdc = report.find('hdc1080')
     temp = hdc.find('t').text
     temp = temp.replace(',', '.')
     y_val.append(float(temp))
 
 plt.xticks(x_val, x_tick, rotation = 90)
-print(x_val)
-plt.grid()
-plt.plot(x_val, y_val)
+plt.grid(linestyle='dashed')
+plt.plot(x_val, y_val, 'r1') # line style
 y_mean = numpy.mean(y_val)
 ax = plt.gca()
-ax.axhline(y=y_mean - 5, color='k')
-ax.axvline(x=0, color='k')
+ax.axhline(y = y_mean - 5, color = 'k')
+ax.axvline(x = 0, color = 'k')
 
 mng = plt.get_current_fig_manager()
 mng.window.state('zoomed')
