6474d5e04e4650b6c59f42a121fa1b857a95e151
Vyacheslav Konovalov <konovalov@healbe.com>
2019-02-26 16:03:10 +0300

    modify flash.py;

--- other/flash.py
+++ other/flash.py
@@ -6,15 +6,19 @@ import getopt
 
 
 def usage():
-    print("""Usage: %s [-h] [-p port]
+    print("""Usage: %s [-h] [-p port] [--path]
     -h          This help
     -p          Bossa port
+    -path       Path to bin
     """ % sys.argv[0])
 
 
 def main():
+    # Defaults
+    func_path: str = '"' + os.getcwd() + '\\SAM4\\Sensor (SAM4SD16C).bin"'
+    
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'p:', ["port="])
+        opts, args = getopt.getopt(sys.argv[1:], 'p:', ["port=", "path="])
     except getopt.GetoptError as err:
         usage() # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
@@ -24,6 +28,8 @@ def main():
     for o, a in opts:
         if o in ("-p", "--port"):
             port = a
+        elif o in ("--path"):
+            func_path = '"' + a + '"'
         else:
             return print("Undefined param" + o)
 
@@ -33,7 +39,7 @@ def main():
         port = 'COM4'
         
     bossa_path: str = 'C:\\Program Files\\BOSSA\\bossac.exe'
-    func_path: str = '"' + os.getcwd() + '\\SAM4\\Sensor (SAM4SD16C).bin"'
+    
     print(bossa_path)
     print(func_path)
     args = [bossa_path + ' -e -w -b -p '+ port + ' ' + func_path]
