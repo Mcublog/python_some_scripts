a20799c94570914c9cd85e4762210eb4b2bc9b91
Vyacheslav Konovalov <konovalov@healbe.com>
2019-05-22 16:35:09 +0300

    modify speed_test.py;

--- graph/speed_test/speed_test.py
+++ graph/speed_test/speed_test.py
@@ -12,21 +12,50 @@ import speed_test_parse as stp
 # block_size = '256'
 # block_size = '120'
 
+# block_size = '4096o'
+# block_size = '2048o'
+# block_size = '1024o'
+# block_size = '512o'
+# block_size = '256o'
+# block_size = '120o'
+
 # block_size = '4096r'
 # block_size = '2048r'
 # block_size = '1024r'
 # block_size = '512r'
-block_size = '256r'
+# block_size = '256r'
 # block_size = '120r'
 
+block_size = '4096p'
+# block_size = '2048p'
+# block_size = '1024p'
+# block_size = '512p'
+# block_size = '256p'
+# block_size = '120p'
 
-f = open(os.getcwd() + '\\test\\' + block_size +'.txt', 'r')
-data = f.read()
-f.close()
+try:
+    if block_size == '':
+        print("block_size not select")
+        raise SystemExit(0)
+except:
+    print("Select block_size")
+    raise SystemExit(0)
 
-read = True if 'r' in block_size else False
-if read:
+try:
+    with open(os.getcwd() + '\\test\\' + block_size +'.txt', 'r') as f:
+        data = f.read()
+except:
+    print("File not found: + " + os.getcwd() + '\\test\\' + block_size +'.txt')
+    raise SystemExit(0)
+
+test_type = 'write'
+
+if 'r' or 'p' in block_size:
+    test_type = 'read'
     block_size = block_size[:-1]
+elif 'o':
+    block_size = block_size[:-1]
+    
 
 time_start = stp.get_param(data, 'start time')
 time_stop = stp.get_param(data, 'stop time')
@@ -38,10 +67,10 @@ print(block_size)
 speeds = []
 for start, stop in zip(time_start, time_stop):
     difftime = int(stop) - int(start)
-    if difftime <= 0:
-        difftime = 1
-    speeds.append(float(block_size) * 1000 / float(difftime))
+    if not difftime <= 0:
+        speeds.append(float(block_size) * 1000 / (float(difftime)))
 
+#speeds = speeds[:10]
 # Create plots
 fig, ax = plt.subplots()
 fig.canvas.set_window_title(block_size)
@@ -49,7 +78,7 @@ plt.xlabel("Block number", fontsize = 12)
 
 # Generate a list from 0 to speeds len
 xdata = range(len(speeds))
-plt.title('LittleFS write speed test (4MB filesize)', fontsize = 14)
+plt.title('LittleFS ' + test_type +' speed test (4MB filesize)', fontsize = 14)
 plt.ylabel('Bytes\\s', fontsize = 12)
   
 ax.plot(xdata, speeds, 'b-', label = block_size + ' byte block')
